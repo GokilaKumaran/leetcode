56d640649c00e6c192679e3c545d0c8238d30ddf
GokilaKumaran <gokilakumarn@gmail.com>
2025-09-12 13:34:30 +0530

    Time: 504 ms (10.09%), Space: 95 MB (69.08%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
@@ -1,23 +1,33 @@
 class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> Double.compare(
-                (double)(b[0] + 1) / (b[1] + 1) - (double)b[0] / b[1],
-                (double)(a[0] + 1) / (a[1] + 1) - (double)a[0] / a[1]
+        PriorityQueue<int []>pq = new PriorityQueue<>(
+            (a,b)->Double.compare(
+                gain(b[0],b[1]),
+                gain(a[0],a[1])
             )
         );
-        for (int[] c : classes)
-            pq.offer(c);
-        while (extraStudents-- > 0){
-            int[] c = pq.poll();
-            c[0]++;
-            c[1]++;
-            pq.offer(c);
+
+        for(int c[] : classes){
+            pq.add(new int[] {c[0],c[1]});
         }
-        double sum = 0.0;
-        for (int[] c : classes){
-            sum += (double)c[0] / c[1];
+
+        while (extraStudents>0){
+            int c[] = pq.poll();
+            int pass = c[0];
+            int total = c[1];
+            pq.add(new int[] {pass+1,total+1});
+            extraStudents--;
         }
-        return sum / classes.length;
+        double ans = 0.0;
+        while(!pq.isEmpty()){
+            int arr[] = pq.poll();
+            ans+=(double)arr[0]/arr[1];
+        }
+
+        return ans/classes.length;
+    }
+
+    public double gain(int pass, int total){
+        return (double)(pass+1)/(total+1) - (double)pass/total;
     }
 }
