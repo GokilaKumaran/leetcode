0ce5c7a213c45075144f986f60bff53241de2835
GokilaKumaran <gokilakumarn@gmail.com>
2025-09-15 14:21:41 +0530

    Time: 5 ms (98.46%), Space: 43.5 MB (42.78%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -3,25 +3,23 @@ class Solution {
       String[] strNums = new String[nums.length];
         for(int i = 0; i < nums.length; i++) {
             strNums[i] = String.valueOf(nums[i]);
-        }
-        
-        // Custom comparator: compare concatenated strings in both possible orders
+        }       
         Arrays.sort(strNums, new Comparator<String>() {
             @Override
             public int compare(String a, String b) {
                 String order1 = a + b;
                 String order2 = b + a;
-                // Sort in descending order based on which concatenation is bigger
+              
                 return order2.compareTo(order1);
             }
         });
         
-        // Edge case: if the largest number is "0", return "0"
+        
         if(strNums[0].equals("0")) {
             return "0";
         }
         
-        // Build the largest number by concatenation
+        
         StringBuilder sb = new StringBuilder();
         for(String s : strNums) {
             sb.append(s);
